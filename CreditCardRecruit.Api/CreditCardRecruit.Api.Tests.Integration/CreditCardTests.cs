4d93686510136b56a1819772ec8a9f9486f46b0c
Kawindu Lokuge <kawindu@outlook.com>
2021-06-01 22:22:17 +1200

    New - setting up initial projects including test projects

---
+++ CreditCardRecruit.Api/CreditCardRecruit.Api.Tests.Integration/CreditCardTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CreditCardRecruit.Api.Tests.Integration
+{
+    public class CreditCardTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CreditCardRecruit.Api/CreditCardRecruit.Api.Tests.Unit/CreditCardTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CreditCardRecruit.Api.Tests.Unit
+{
+    public class CreditCardTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
